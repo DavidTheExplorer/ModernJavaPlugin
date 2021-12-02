b6715e9bb10f9a368bb34fd38430d8ef6d958682
David <darknight4123@gmail.com>
2021-12-02 22:24:41 +0200

    Uploading the project

---
+++ src/main/java/dte/modernjavaplugin/ModernJavaPlugin.java
@@ -0,0 +1,75 @@
+package dte.modernjavaplugin;
+
+import java.util.Arrays;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ModernJavaPlugin extends JavaPlugin
+{
+	private final String pluginName = getDescription().getName();
+	
+	private boolean isDisabled;
+	private Runnable safeDisableListener;
+	
+	/**
+	 * Registers the provided {@code listeners} for this plugin.
+	 * 
+	 * @param listeners The listeners to register.
+	 */
+	public void registerListeners(Listener... listeners) 
+	{
+		for(Listener listener : listeners)
+			Bukkit.getPluginManager().registerEvents(listener, this);
+	}
+	
+	/**
+	 * Sends a colorable message to the console, without omitting the plugin's prefix.
+	 * 
+	 * @param message The message to send.
+	 */
+	public void logToConsole(String message) 
+	{
+		String withPrefix = String.format("[%s] %s", this.pluginName, message);
+
+		Bukkit.getConsoleSender().sendMessage(withPrefix);
+	}
+	
+	
+	
+	/*
+	 * Safe Disable Methods - The only workaround the following example scenario:
+	 * 1. During onEnabe() You save the instance of Essentials.
+	 * 2. During onDisable() you try to send a message to all the AFK players.
+	 * 3. NPE is suddenly thrown during onDisable() because Essentials wasn't on the server.
+	 * 
+	 * The status of the plugin must be saved, because onDisable() is called *after* the plugin is disabled - so isEnabled() will always be false.
+	 */
+	
+	@Override
+	public void onDisable()
+	{
+		if(this.isDisabled) 
+			return;
+		
+		this.safeDisableListener.run();
+	}
+	
+	public void disable() 
+	{
+		this.isDisabled = true;
+		setEnabled(false);
+	}
+	
+	public void disableWithError(String... messages) 
+	{
+		Arrays.stream(messages).forEach(this::logToConsole);
+		disable();
+	}
+	
+	protected void setDisableListener(Runnable code) 
+	{
+		this.safeDisableListener = code;
+	}
+}
